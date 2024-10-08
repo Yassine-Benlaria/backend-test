249926707da2436852d4c94dd253d72e8a2becfd
Yassine-Benlaria <benlariay@gmail.com>
2024-10-08 23:15:18 +0100

    Add Swagger documentation for API endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //validtion pipe
   app.useGlobalPipes(new ValidationPipe());
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Product Management API')
+    .setDescription('The Product Management API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,42 +3,50 @@ import { UsersService } from './users.service';
 import { UpdateUserBody } from './dtos/queries/update-user.body';
 import { CreateUserBody } from './dtos/queries/create-user.body';
 import { UserRole } from './user.schema';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('client')
+  @ApiOperation({ summary: 'Create a client' })
   async createClient(@Body() body: CreateUserBody) {
     return this.usersService.create({ ...body, role: UserRole.CLIENT });
   }
 
   @Post('manager')
+  @ApiOperation({ summary: 'Create a manager' })
   async createManager(@Body() body: CreateUserBody) {
     return this.usersService.create({ ...body, role: UserRole.MANAGER });
   }
 
   @Post('admin')
+  @ApiOperation({ summary: 'Create an admin' })
   async createAdmin(@Body() body: CreateUserBody) {
     return this.usersService.create({ ...body, role: UserRole.ADMIN });
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get users' })
   async getUsers() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a user by id' })
   async getUser(id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a user by id' })
   async updateUser(id: string, @Body() body: UpdateUserBody) {
     return this.usersService.update(id, body);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user by id' })
   async deleteUser(id: string) {
     return this.usersService.remove(id);
   }
